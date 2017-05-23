5eedf0ecb0f556d690faf35ff0cb9ebc468f52ae
Ubuntu <ubuntu@ip-172-31-4-13.us-west-2.compute.internal>
2017-05-23 02:09:30 +0000

    modified upload.sh to include date time in its filename to be uplaoded to s3

--- upload.sh
+++ upload.sh
@@ -1,2 +1,3 @@
-(printf "vm-worker: " && hostname -i && time aws s3 cp $1 s3://tcss-562/upload/ && echo "========")  &>> ./output/upload_output.txt
+today=`date +%Y-%m-%d.%H:%M:%S`
+(printf "vm-worker: " && hostname -i && time aws s3 cp $1 s3://tcss-562/upload/$today"-"$1 && echo "========")  &>> ./output/upload_output.txt
 aws s3 cp ./output/upload_output.txt s3://tcss-562/upload/output-$HOSTNAME
