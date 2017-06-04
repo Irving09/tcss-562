347e3a9cdf104cca5797cdc958c454339c0531a8
Ubuntu <ubuntu@ip-172-31-4-13.us-west-2.compute.internal>
2017-06-04 19:51:35 +0000

    added time to upload.sh in nanoseconds

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 today=`date +%Y-%m-%d.%H:%M:%S`
-(printf "vm-worker: " && hostname -i && time aws s3 cp $1 s3://tcss-562/upload/$HOSTNAME"-"$1 && echo "========")  &>> ./output/upload_output.txt
+(printf "vm-worker: $(hostname -i)" && start=$(date +%s%N) && aws s3 cp $1 s3://tcss-562/upload/$HOSTNAME"-"$1 && end=$(date +%s%N) && echo "$((end - start))ns" && echo "========")  &>> ./output/upload_output.txt
 aws s3 cp ./output/upload_output.txt s3://tcss-562/upload/output-$HOSTNAME
