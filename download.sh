3c6b1cb8e3b9862fbc899065d3b0ee75e244b0b7
Ubuntu <ubuntu@ip-172-31-4-13.us-west-2.compute.internal>
2017-06-04 17:11:23 +0000

    added ns time to s3 download

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-(printf "vm-worker: " && hostname -i && time aws s3 cp s3://tcss-562/download/$1 ./output && echo "========")  &>> ./output/download_output.txt
+(printf "vm-worker: $(hostname -i)" && start=$(date +%s%N) && aws s3 cp s3://tcss-562/download/$1 ./output && end=$(date +%s%N) && echo "$((end - start))ns"&& echo "========")  &>> ./output/download_output.txt
 aws s3 cp ./output/download_output.txt s3://tcss-562/download/output-$HOSTNAME
