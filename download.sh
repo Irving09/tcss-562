9d0aff8b88fffac9233bb79e3d718b1d5f0433aa
irving09 <innoirvinge@gmail.com>
2017-06-04 20:33:45 +0000

    adding public read to upload tests in S3

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 (printf "vm-worker: $(hostname -i)" && echo "" && start=$(date +%s%N) && aws s3 cp s3://tcss-562/$1 ./output && end=$(date +%s%N) && echo "$((end - start))ns")  &>> ./output/download_output.txt
-aws s3 cp ./output/download_output.txt s3://tcss-562/download/output-$HOSTNAME
+aws s3 cp ./output/download_output.txt s3://tcss-562/download/output-$HOSTNAME --acl public-read
 tail -1 ./output/download_output.txt &>> ./output/download_speed.$1.txt
-aws s3 cp ./output/download_speed.$1.txt s3://tcss-562/download/download_speed.$HOSTNAME.$1.txt
+aws s3 cp ./output/download_speed.$1.txt s3://tcss-562/download/download_speed.$HOSTNAME.$1.txt --acl public-read
--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 today=`date +%Y-%m-%d.%H:%M:%S`
 (printf "vm-worker: $(hostname -i)" && echo "" && start=$(date +%s%N) && aws s3 cp $1 s3://tcss-562/upload/$HOSTNAME"-"$1 && end=$(date +%s%N) && echo "$((end - start))ns")  &>> ./output/upload_output.txt
-aws s3 cp ./output/upload_output.txt s3://tcss-562/upload/output-$HOSTNAME
+aws s3 cp ./output/upload_output.txt s3://tcss-562/upload/output-$HOSTNAME --acl public-read
 tail -1 ./output/upload_output.txt &>> ./output/upload_speed.$1.txt
-aws s3 cp ./output/upload_speed.$1.txt s3://tcss-562/upload/upload_speed.$HOSTNAME.$1.txt
+aws s3 cp ./output/upload_speed.$1.txt s3://tcss-562/upload/upload_speed.$HOSTNAME.$1.txt --acl public-read
